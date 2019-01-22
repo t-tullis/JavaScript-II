f680dad569921b5d6a153c82ce83459b6f9c8799
t-tullis <t.tullis34@gmail.com>
2019-01-22 15:46:45 -0800

    Finished Challenges

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,6 +1,7 @@
 // Create a callback function and invoke the function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const sportsTeams = ['Giants', 'Warriors', 'Lakers', 'Knicks']
 
 /* 
 
@@ -21,27 +22,64 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 */
 
+//higher order function for arr problems
+function higherOrderFunction1(arr, cb){
+  console.log(cb(arr))
+}
+
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return arr.length;
 }
+higherOrderFunction1(items, getLength);
+
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  const lastItem = arr.length - 1;
+  return arr[lastItem];
+}
+higherOrderFunction1(items, last);
+
+//higher order function for Number problems
+function higherOrderFunction2(x, y, cb){
+  console.log(cb(x, y))
 }
 
-function sumNums(x, y, cb) {
+function sumNums(x, y) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return x + y;
 }
+higherOrderFunction2(4, 9, sumNums);
 
-function multiplyNums(x, y, cb) {
+
+
+function multiplyNums(x, y) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return x * y
+}
+higherOrderFunction2(4, 3, multiplyNums);
+
+//Higher Order Function to check list;
+function higherOrderFunction3(item,list,cb){
+  console.log(cb(item, list))
 }
 
-function contains(item, list, cb) {
+function contains(item,list) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  for(let i = 0; i < list.length; i++){
+    if(item.toLowerCase() === list[i].toLowerCase()){
+      return true;
+    }
+  }
+  return false;
 }
+
+higherOrderFunction3('yo-yo',items, contains)
+higherOrderFunction3('giants',sportsTeams, contains)
+
 
 /* STRETCH PROBLEM */
 
